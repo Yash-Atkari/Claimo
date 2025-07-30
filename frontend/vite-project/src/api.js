3ea81734dca76888260ba062e63200927aea5de2
Yash Atkari <yashatkari7@gmail.com>
2025-07-30 16:06:56 +0530

    Adjust url

--- frontend/vite-project/src/api.js
+++ frontend/vite-project/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export const getUsers      = () => axios.get(`/api/users`);
-export const addUser       = (name) => axios.post(`/api/users/${name}`);
-export const claimPoints   = (userId) => axios.post(`/api/claims/${userId}`);
-export const getLeaderboard = () => axios.get(`/api/claims/leaderboard`);
+const BASE_URL = import.meta.env.VITE_API_BASE;
+
+export const getUsers = () => axios.get(`${BASE_URL}/api/users`);
+export const addUser = (name) => axios.post(`${BASE_URL}/api/users/${name}`);
+export const claimPoints = (userId) => axios.post(`${BASE_URL}/api/claims/${userId}`);
+export const getLeaderboard = () => axios.get(`${BASE_URL}/api/claims/leaderboard`);
